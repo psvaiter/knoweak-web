7a347cb5b0bb9284998ef09f0b3c4e7860f8b127
Patrick Svaiter <psvaiter@gmail.com>
2018-04-01 15:45:02 -0300

    Adiciona roteamento do Angular para navegar entre as páginas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DepartmentComponent } from './components/department/department.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'department', component: DepartmentComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     NgbModule.forRoot(),
     AngularFontAwesomeModule
   ],
