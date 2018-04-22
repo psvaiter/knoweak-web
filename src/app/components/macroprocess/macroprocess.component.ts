071e5b1b9dc6a4c5097174156e0c337f0219fd69
Patrick Svaiter <psvaiter@gmail.com>
2018-04-22 18:08:10 -0300

    Implementa componente de macroprocesso utilizando componente de CRUD.

--- src/app/components/macroprocess/macroprocess.component.ts
+++ src/app/components/macroprocess/macroprocess.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Paging } from '../pagination/pagination.component';
+import { CrudComponent } from '../utils/crud/crud.component';
+import { CrudService } from '../utils/crud/crud.service';
 
 @Component({
   selector: 'app-macroprocess',
   templateUrl: './macroprocess.component.html',
   styleUrls: ['./macroprocess.component.scss']
 })
-export class MacroprocessComponent implements OnInit {
 
-  constructor() { }
+export class MacroprocessComponent extends CrudComponent<Macroprocess> implements OnInit {
+
+  url = CrudService.BaseUrl + '/macroprocesses';
 
   ngOnInit() {
+    this.getRecords(1);
   }
+}
 
+class Macroprocess {
+  id: number;
+  name: string;
+  createdOn: DateTimeFormat;
+  lastModifiedOn: DateTimeFormat;
 }
