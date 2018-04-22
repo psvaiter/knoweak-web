10c08b1715516dae04010022653c63126d7bd879
Patrick Svaiter <psvaiter@gmail.com>
2018-04-22 18:17:02 -0300

    Implementa componente para Ativos de TI.

--- src/app/components/it-asset/it-asset.component.ts
+++ src/app/components/it-asset/it-asset.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudComponent } from '../utils/crud/crud.component';
+import { CrudService } from '../utils/crud/crud.service';
 
 @Component({
   selector: 'app-it-asset',
   templateUrl: './it-asset.component.html',
   styleUrls: ['./it-asset.component.scss']
 })
-export class ItAssetComponent implements OnInit {
 
-  constructor() { }
+export class ItAssetComponent extends CrudComponent<ItAsset> implements OnInit {
+
+  url = CrudService.BaseUrl + '/itAssets';
 
   ngOnInit() {
+    this.getRecords(1);
   }
 
+}
+
+class ItAsset {
+  id: number;
+  name: string;
+  createdOn: DateTimeFormat;
+  lastModifiedOn: DateTimeFormat;
 }
