15927b1e6a95415589c3d699117fea3267135a96
Patrick Svaiter <psvaiter@gmail.com>
2018-04-22 18:19:38 -0300

    Implementa componente para Serviços de TI.

--- src/app/components/it-service/it-service.component.ts
+++ src/app/components/it-service/it-service.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudComponent } from '../utils/crud/crud.component';
+import { CrudService } from '../utils/crud/crud.service';
 
 @Component({
   selector: 'app-it-service',
   templateUrl: './it-service.component.html',
   styleUrls: ['./it-service.component.scss']
 })
-export class ItServiceComponent implements OnInit {
 
-  constructor() { }
+export class ItServiceComponent extends CrudComponent<ItService> implements OnInit {
+
+  url = CrudService.BaseUrl + '/itServices';
 
   ngOnInit() {
+    this.getRecords(1);
   }
 
+}
+
+class ItService {
+  id: number;
+  name: string;
+  createdOn: DateTimeFormat;
+  lastModifiedOn: DateTimeFormat;
 }
