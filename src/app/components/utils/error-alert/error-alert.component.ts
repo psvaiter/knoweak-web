717d487d69285bfdf49ca88807970f670e4b0824
Patrick Svaiter <psvaiter@gmail.com>
2018-04-22 18:03:30 -0300

    Implementa alerta de erros como componente.

---
+++ src/app/components/utils/error-alert/error-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-alert',
+  templateUrl: './error-alert.component.html',
+  styleUrls: ['./error-alert.component.scss']
+})
+
+export class ErrorAlertComponent {
+
+  @Input() errors = [];
+
+  constructor() { }
+  
+}
