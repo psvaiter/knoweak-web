3b889ecf10b21e2203783bc22772f2813e831dfd
Patrick Svaiter <psvaiter@gmail.com>
2018-04-24 23:30:25 -0300

    Implementa componente para Ameaças.

--- src/app/components/security-threat/security-threat.component.ts
+++ src/app/components/security-threat/security-threat.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudComponent } from '../utils/crud/crud.component';
+import { CrudService } from '../utils/crud/crud.service';
 
 @Component({
   selector: 'app-security-threat',
   templateUrl: './security-threat.component.html',
   styleUrls: ['./security-threat.component.scss']
 })
-export class SecurityThreatComponent implements OnInit {
 
-  constructor() { }
+export class SecurityThreatComponent extends CrudComponent<SecurityThreat> implements OnInit {
 
+  url = CrudService.BaseUrl + '/securityThreats';
+ 
   ngOnInit() {
+    this.getRecords(1);
   }
 
+}
+
+class SecurityThreat {
+  id: number;
+  name: string;
+  description: string;
+  createdOn: DateTimeFormat;
+  lastModifiedOn: DateTimeFormat;
 }
