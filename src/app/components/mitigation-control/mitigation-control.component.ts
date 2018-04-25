64d9cfb74828c59894c5b84798fd05ffcdbaa977
Patrick Svaiter <psvaiter@gmail.com>
2018-04-24 23:36:42 -0300

    Implementa componente para Controles Mitigatórios.

--- src/app/components/mitigation-control/mitigation-control.component.ts
+++ src/app/components/mitigation-control/mitigation-control.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudComponent } from '../utils/crud/crud.component';
+import { CrudService } from '../utils/crud/crud.service';
 
 @Component({
   selector: 'app-mitigation-control',
   templateUrl: './mitigation-control.component.html',
   styleUrls: ['./mitigation-control.component.scss']
 })
-export class MitigationControlComponent implements OnInit {
 
-  constructor() { }
-
+export class MitigationControlComponent extends CrudComponent<MitigationControl> implements OnInit {
+ 
+  url = CrudService.BaseUrl + '/mitigationControls';
+ 
   ngOnInit() {
+    this.getRecords(1);
   }
 
+}
+
+class MitigationControl {
+  id: number;
+  name: string;
+  description: string;
+  createdOn: DateTimeFormat;
+  lastModifiedOn: DateTimeFormat;
 }
