b81ed45c176f9714f670c8abc0edbd4543eb2707
Patrick Svaiter <psvaiter@gmail.com>
2018-05-06 23:10:39 -0300

    Inicia implementação de tela da estrutura de uma organização.

--- src/app/components/organization/organization-structure/organization-structure.component.ts
+++ src/app/components/organization/organization-structure/organization-structure.component.ts
@@ -5,11 +5,48 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './organization-structure.component.html',
   styleUrls: ['./organization-structure.component.scss']
 })
+
 export class OrganizationStructureComponent implements OnInit {
+
+  departments = [];
+  organization: Organization;
+  sectionExpanded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.organization = <Organization> {
+      
+      legalName: "Nome Empresarial",
+      tradeName: "Nome Fantasia",
+      departments: [
+        {
+          id: 1,
+          name: "Department 1",
+          macroprocesses: [
+            
+          ]
+        },
+        {
+          id: 2,
+          name: "Department 2",
+          macroprocesses: [
+            { id: 1, name: "Macroprocesso de teste 1" }
+          ]
+        },
+        {
+          id: 3,
+          name: "Department 3"
+        },
+        {
+          id: 4,
+          name: "Department 4"
+        }
+      ]
+    };
+
   }
 
 }
+
+
--- src/app/components/organization/organization.ts
+++ src/app/components/organization/organization.ts
@@ -5,4 +5,39 @@ class Organization {
     tradeName: string;
     createdOn: DateTimeFormat;
     lastModifiedOn: DateTimeFormat;
+    departments: OrganizationDepartment[] = [];
 }
+
+class OrganizationDepartment {
+    id: number;
+    name: string;
+    macroprocesses: OrganizationMacroprocess[] = [];
+    expanded: boolean = false;
+}
+  
+class OrganizationMacroprocess {
+    id: number;
+    name: string;
+    processes: OrganizationProcess[] = [];
+    expanded: boolean = false;
+}
+  
+class OrganizationProcess {
+    id: number;
+    name: string;
+    itServices: OrganizationItService[] = [];
+    expanded: boolean = false;
+}
+  
+class OrganizationItService {
+    id: number;
+    name: string;
+    itAssets: OrganizationItAsset[] = [];
+    expanded: boolean = false;
+}
+  
+class OrganizationItAsset {
+    id: number;
+    name: string;
+}
+  
