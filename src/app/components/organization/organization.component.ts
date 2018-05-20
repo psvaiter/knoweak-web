860d4f678be9a4af2783578117fb0bcb7685499a
Patrick Svaiter <psvaiter@gmail.com>
2018-05-20 19:59:59 -0300

    Atualiza dependências.

--- src/app/components/organization/organization.component.ts
+++ src/app/components/organization/organization.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudComponent } from '../utils/crud/crud.component';
 import { CrudService } from '../utils/crud/crud.service';
+import { Organization } from './organization';
 
 @Component({
   selector: 'app-organization',
