a267eb1bf4462401ca701f91a436b06ba26225c3
Patrick Svaiter <psvaiter@gmail.com>
2018-07-23 20:42:56 -0300

    Setup environment vars and adds .netlify config file.

--- src/app/components/utils/crud/crud.service.ts
+++ src/app/components/utils/crud/crud.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class CrudService {
 
-  static readonly BaseUrl: string = 'http://localhost:8000';
+  static readonly BaseUrl: string = environment.apiBaseUrl;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
