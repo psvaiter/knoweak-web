6ff5d8bae4798a5285462e9656294305564b94c6
Patrick Svaiter <psvaiter@gmail.com>
2018-07-24 20:57:55 -0300

    Prints user info in dashboard and redirects to it on login.

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import * as auth0 from 'auth0-js';
 import { environment } from '../../../environments/environment';
+import * as auth0 from 'auth0-js';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 (window as any).global = window;
 
@@ -17,6 +18,8 @@ export class AuthService {
     scope: 'openid profile'
   });
 
+  jwt = new JwtHelperService();
+
   constructor(public router: Router) { }
 
   public login() {
@@ -30,7 +33,7 @@ export class AuthService {
       if (authResult && authResult.accessToken && authResult.idToken) {
         window.location.hash = '';
         this.setSession(authResult);
-        this.router.navigate(['/organizations']);
+        this.router.navigate(['/dashboard']);
       }
       else if (err) {
         this.router.navigate(['/']);
@@ -46,6 +49,10 @@ export class AuthService {
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
+
+    // Get info from ID token
+    let idToken = this.jwt.decodeToken(authResult.idToken);
+    localStorage.setItem('name', idToken.name);
   }
 
   public logout(): void {
