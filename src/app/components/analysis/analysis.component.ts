164994b70c0f8d83ef65f89f7532c23f68013c8e
Patrick Svaiter <psvaiter@gmail.com>
2018-07-26 20:54:03 -0300

    Implements component to list organization analyses.

--- src/app/components/analysis/analysis.component.ts
+++ src/app/components/analysis/analysis.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudComponent } from '../utils/crud/crud.component';
+import { CrudService } from '../utils/crud/crud.service';
+import { Analysis } from './analysis';
+import { ActivatedRoute } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-analysis',
   templateUrl: './analysis.component.html',
   styleUrls: ['./analysis.component.scss']
 })
-export class AnalysisComponent implements OnInit {
+export class AnalysisComponent extends CrudComponent<Analysis> implements OnInit {
 
-  constructor() { }
+  organizationId: number;
+  
+  constructor(
+    protected crudService: CrudService,
+    private route: ActivatedRoute) {
+      super(crudService);
+      route.params.subscribe(params => {
+        this.organizationId = params['id'];
+        this.url = `${CrudService.BaseUrl}/organizations/${this.organizationId}/analyses`;
+      });
+  }
 
   ngOnInit() {
+    this.getRecords(1);
   }
 
 }
---
+++ src/app/components/analysis/analysis.ts
@@ -0,0 +1,10 @@
+export class Analysis {
+    id: number;
+    description: string;
+
+    details: AnalysisDeatil[] = [];
+}
+
+export class AnalysisDeatil {
+
+}
