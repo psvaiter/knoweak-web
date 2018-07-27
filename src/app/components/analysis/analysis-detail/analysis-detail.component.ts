024ec51cf2d481b48bcc3c4d4d926d521304a308
Patrick Svaiter <psvaiter@gmail.com>
2018-07-27 01:32:54 -0300

    Creates analysis detail component.

---
+++ src/app/components/analysis/analysis-detail/analysis-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CrudComponent } from '../../utils/crud/crud.component';
+import { Analysis, AnalysisDeatil } from '../analysis';
+import { CrudService } from '../../utils/crud/crud.service';
+
+@Component({
+  selector: 'app-analysis-detail',
+  templateUrl: './analysis-detail.component.html',
+  styleUrls: ['./analysis-detail.component.scss']
+})
+export class AnalysisDetailComponent extends CrudComponent<Analysis> implements OnInit {
+
+  analysisId: number;
+  organizationId: number;
+
+  constructor(
+    private crudService: CrudService,
+    private route: ActivatedRoute
+  ) { 
+    super(crudService);
+    route.paramMap.subscribe(params => {
+      this.organizationId = +params.get("id");
+      this.analysisId = +params.get("analysisId");
+      this.url = `${CrudService.BaseUrl}/organizations/${this.organizationId}/analyses/${this.analysisId}`;
+    });
+  }
+
+  ngOnInit() {
+    this.getRecords(1);
+  }
+
+}
