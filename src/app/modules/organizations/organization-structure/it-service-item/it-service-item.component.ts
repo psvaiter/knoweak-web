703c428287997c411c04b77f56d448a85f7640cd
Patrick Svaiter <psvaiter@gmail.com>
2018-11-19 20:57:25 -0200

    Shows IT services when expanding processes.

--- src/app/modules/organizations/organization-structure/it-service-item/it-service-item.component.ts
+++ src/app/modules/organizations/organization-structure/it-service-item/it-service-item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+
+import { OrganizationItService, OrganizationItAsset } from '../../organization/organization';
+import { OrganizationItAssetService } from '../../../../services/api/organization/organization-it-asset.service';
 
 @Component({
   selector: 'app-it-service-item',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItServiceItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() itService: OrganizationItService;
+  @Output() delete = new EventEmitter();
+
+  expanded: boolean;
+  organizationId: number;
+  itAssets: OrganizationItAsset[];
+
+  constructor(
+    private modalService: BsModalService,
+    private organizationItAssetService: OrganizationItAssetService
+  ) { }
 
   ngOnInit() {
+    this.organizationId = this.itService.organizationId;
+  }
+
+  toggleItAssets() {
+    this.expanded = !this.expanded;
+    if (!this.expanded) {
+      return;
+    }
+    this.listItServiceItAssets();
+  }
+
+  deleteItService() {
+    // Emit event asking for parent component to remove from its register
+    this.delete.emit(this.itService);
+  }
+
+  private listItServiceItAssets() {
+    
   }
 
 }
