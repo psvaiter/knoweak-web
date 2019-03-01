d8406f6dbde5e2e1fdb5f0904bc021ec474e0153
Patrick Svaiter <psvaiter@gmail.com>
2019-03-01 01:33:30 -0300

    Updates localhost port.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:8000',
+  apiBaseUrl: 'http://localhost:8001',
   auth0: {
     clientID: 'kC8r2l8yfy398pJuDuAXBudEWe3l7Hlt',
     domain: 'knoweak.auth0.com',
